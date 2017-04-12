928f7e715df78c944b6268db40339ceb0182dbd1
nanakiksc <polcusco@gmail.com>
2017-04-12 11:02:58 +0200

    Small fix in CV.

--- train.R
+++ train.R
@@ -4,13 +4,13 @@ input <- scale(iris[-5])
 labels <- matrix(c(iris[5] == 'setosa', iris[5] == 'virginica', iris[5] == 'versicolor'), ncol = 3)
 
 n.iter.range <- c(2, 4)
-alpha.range <- c(-1, -4)
+alpha.range <- c(-4, -1)
 lambda.range <- c(-3, 3)
 breadth.vec <- (1 * ncol(input)):(4 * ncol(input))
 depth.vec <- 0:3
-n.samples <- 10
+n.samples <- 1
 
-sample.param <- function(range) runif(1, min = 10^range[1], max = 10^range[2])
+sample.param <- function(range) runif(1, min = 10^min(range), max = 10^max(range))
 
 cv.results <- data.frame()
 for (i in 1:n.samples) {
